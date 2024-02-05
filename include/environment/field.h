c788694d1a54c86c1e6f50d3de5de87b3e76727d
IbrahimSquared <ibrahim.ibrahim@kuleuven.be>
2024-02-05 13:53:36 +0100

    add resize to field

--- include/environment/field.h
+++ include/environment/field.h
@@ -6,35 +6,43 @@
 
 namespace vbs {
 
-template <typename T>
-class Field {
-  
-  public:
-    using size_t = std::size_t;
-    Field() : nx_(0), ny_(0), size_(0), data_(nullptr) {}
+template <typename T> class Field {
 
-    explicit Field(size_t nx, size_t ny, T default_value) : nx_(nx), ny_(ny), size_(nx * ny) {
-      data_ = std::make_unique<T[]>(size_);
-      for (size_t i = 0; i < size_; ++i) {
-        data_[i] = default_value;
-      }
+public:
+  using size_t = std::size_t;
+  Field() : nx_(0), ny_(0), size_(0), data_(nullptr) {}
+
+  explicit Field(size_t nx, size_t ny, T default_value)
+      : nx_(nx), ny_(ny), size_(nx * ny) {
+    data_ = std::make_unique<T[]>(size_);
+    for (size_t i = 0; i < size_; ++i) {
+      data_[i] = default_value;
     }
+  }
 
-    void set(size_t x, size_t y, T value) { data_[x + y * nx_] = value; }
-    T get(size_t x, size_t y) const { return data_[x + y * nx_]; }
-    size_t nx() const { return nx_; }
-    size_t ny() const { return ny_; }
-    size_t size() const { return size_; }
+  void set(size_t x, size_t y, T value) { data_[x + y * nx_] = value; }
+  T get(size_t x, size_t y) const { return data_[x + y * nx_]; }
+  size_t nx() const { return nx_; }
+  size_t ny() const { return ny_; }
+  size_t size() const { return size_; }
 
-    void reset() { 
-      data_ = std::make_unique<T[]>(size_);
+  void reset() { data_ = std::make_unique<T[]>(size_); }
+
+  void resize(size_t nx, size_t ny, T default_value) {
+    nx_ = nx;
+    ny_ = ny;
+    size_ = nx * ny;
+    data_ = std::make_unique<T[]>(size_);
+    for (size_t i = 0; i < size_; ++i) {
+      data_[i] = default_value;
     }
+  }
 
-  private:
-    size_t nx_;
-    size_t ny_;
-    size_t size_;
-    std::unique_ptr<T[]> data_;
+private:
+  size_t nx_;
+  size_t ny_;
+  size_t size_;
+  std::unique_ptr<T[]> data_;
 };
 
 } // namespace vbs
